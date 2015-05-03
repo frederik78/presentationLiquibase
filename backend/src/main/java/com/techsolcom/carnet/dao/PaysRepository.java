9a9a26ff87be717d2dbadf7bccad2a89de0c9019
frederic.minatchy <frederik78@yahoo.fr>
2015-05-03 18:57:36 +0200

    Ajout des tests unitaires dans backend

---
+++ backend/src/main/java/com/techsolcom/carnet/dao/PaysRepository.java
@@ -0,0 +1,11 @@
+package com.techsolcom.carnet.dao;
+
+import com.techsolcom.dto.carnet.Pays;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Frederic on 03/05/15.
+ */
+
+public interface PaysRepository extends JpaRepository<Pays, Integer>{
+}
--- backend/src/test/java/com/techsolcom/carnet/DBTest.java
+++ backend/src/test/java/com/techsolcom/carnet/DBTest.java
@@ -1,4 +1,4 @@
-package com.groupemre.simba2;
+package com.techsolcom.carnet;
 
 import liquibase.Liquibase;
 import liquibase.database.jvm.JdbcConnection;
@@ -10,83 +10,95 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Classe de test générique permettant de charger avec Liquibase une base de données de test, et de la détruire après exécution des tests.
- *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:db-context.xml")
-public abstract class DBTest  {
+public abstract class DBTest {
 
-  public static final String DB_STRUCTURE = "structure.xml";
+    public static final String DB_STRUCTURE = "structure.xml";
+    public static final String DB_DATA = "donnees.xml";
 
-  private static final ResourceAccessor LIQUIBASE_RESOURCE_ACCESSOR = new ClassLoaderResourceAccessor();
+    private static final ResourceAccessor LIQUIBASE_RESOURCE_ACCESSOR = new ClassLoaderResourceAccessor();
 
-  private String[] testDataResources;
+    protected static String[] testDataResources = new String[]{};
 
-  private static final AtomicBoolean structureInit = new AtomicBoolean(false);
+    private static final AtomicBoolean structureInit = new AtomicBoolean(false);
 
-  private static Date structureCreated = null;
+    private static Date structureCreated = null;
 
-  @Autowired
-  private DataSource dataSource;
+    //    @Value("${db.jdbc}")
+    private static String IN_MEMORY_DB = "jdbc:h2:file:~/tmp/carnet;MODE=MySQL";
+    //    @Value("${db.password}")
+    private static String DB_PASSWORD = "sa";
+    //    @Value("${db.user}")
+    private static String DB_LOGIN = "sa";
 
-  /**
-   * Création d'une base de données de test, et chargement d'un jeu de données de test additionnel.
-   *
-   * @param testDataResources Fichier liquibase de chargement des données de test.
-   */
-  public DBTest(String... testDataResources) {
-    this.testDataResources = testDataResources;
-  }
-
-  @BeforeClass
-  public void prepareTestDatabase() throws SQLException, LiquibaseException {
-    if (!structureInit.getAndSet(true)) {
-      createStructure();
-      structureCreated = new Date();
+    /**
+     * Création d'une base de données de test, et chargement d'un jeu de données de test additionnel.
+     *
+     * @param testDataResources Fichier liquibase de chargement des données de test.
+     */
+    public DBTest(String... testDataResources) {
+        this.testDataResources = testDataResources;
     }
-    insertTestData();
-  }
 
-  public void createStructure() throws SQLException, LiquibaseException {
-    try (Connection connection = dataSource.getConnection()) {
-      JdbcConnection jdbcConnection = new JdbcConnection(connection);
-      Liquibase liquibase = new Liquibase(DB_STRUCTURE, LIQUIBASE_RESOURCE_ACCESSOR, jdbcConnection);
-      liquibase.update("");
-    }
-  }
+    @BeforeClass
+    public static void prepareTestDatabase() throws SQLException, LiquibaseException, ClassNotFoundException {
 
-  public void insertTestData() throws SQLException, LiquibaseException {
-    try (Connection connection = dataSource.getConnection()) {
-      JdbcConnection jdbcConnection = new JdbcConnection(connection);
-      for (String testDataResource : testDataResources) {
-        Liquibase liquibase = new Liquibase(testDataResource, LIQUIBASE_RESOURCE_ACCESSOR, jdbcConnection);
-        liquibase.update("");
-      }
+        if (!structureInit.getAndSet(true)) {
+            createStructure();
+            structureCreated = new Date();
+        }
+        insertTestData();
     }
-  }
 
-  @AfterClass
-  public void dropTestData() throws SQLException, LiquibaseException {
-    // Le rollback se fait en ordre inverse d'application.
-    ArrayUtils.reverse(testDataResources);
-    try (Connection connection = dataSource.getConnection()) {
-      JdbcConnection jdbcConnection = new JdbcConnection(connection);
-      for (String testDataResource : testDataResources) {
-        Liquibase liquibase = new Liquibase(testDataResource, LIQUIBASE_RESOURCE_ACCESSOR, jdbcConnection);
-        liquibase.rollback(structureCreated, "");
-      }
+    public static void createStructure() throws SQLException, LiquibaseException, ClassNotFoundException {
+
+        Class.forName("org.h2.Driver");
+        try (Connection connection = DriverManager.getConnection(IN_MEMORY_DB, DB_LOGIN, DB_PASSWORD)) {
+            JdbcConnection jdbcConnection = new JdbcConnection(connection);
+            Liquibase liquibase = new Liquibase(DB_STRUCTURE, LIQUIBASE_RESOURCE_ACCESSOR, jdbcConnection);
+            liquibase.update("");
+
+            liquibase = new Liquibase(DB_DATA, LIQUIBASE_RESOURCE_ACCESSOR, jdbcConnection);
+            liquibase.update("");
+        }
     }
-  }
+
+    public static void insertTestData() throws SQLException, LiquibaseException, ClassNotFoundException {
+        try (Connection connection = DriverManager.getConnection(IN_MEMORY_DB, DB_LOGIN, DB_PASSWORD)) {
+            JdbcConnection jdbcConnection = new JdbcConnection(connection);
+            for (String testDataResource : testDataResources) {
+                Liquibase liquibase = new Liquibase(testDataResource, LIQUIBASE_RESOURCE_ACCESSOR, jdbcConnection);
+                liquibase.update("");
+            }
+        }
+    }
+
+    @AfterClass
+    public static void dropTestData() throws SQLException, LiquibaseException, ClassNotFoundException {
+        // Le rollback se fait en ordre inverse d'application.
+        ArrayUtils.reverse(testDataResources);
+        try (Connection connection = DriverManager.getConnection(IN_MEMORY_DB, DB_LOGIN, DB_PASSWORD)) {
+            JdbcConnection jdbcConnection = new JdbcConnection(connection);
+            for (String testDataResource : testDataResources) {
+                Liquibase liquibase = new Liquibase(testDataResource, LIQUIBASE_RESOURCE_ACCESSOR, jdbcConnection);
+                liquibase.rollback(structureCreated, "");
+            }
+        }
+    }
 }
