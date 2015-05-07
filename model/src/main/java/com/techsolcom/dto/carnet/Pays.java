953d50275ae9403368286d8f201ba44bd626383f
frederic.minatchy <frederik78@yahoo.fr>
2015-05-07 05:47:37 +0200

    Ajout de la classe Pays (mais commentée)

---
+++ model/src/main/java/com/techsolcom/dto/carnet/Pays.java
@@ -0,0 +1,50 @@
+//package com.techsolcom.dto.carnet;
+//
+//
+//
+//import javax.persistence.*;
+//import java.util.Date;
+//import java.util.List;
+//
+//@Entity
+//@Table(name = "pays_pay")
+//public class Pays {
+//
+//
+//
+//    @Id
+//    @Column(name = "pay_id", unique = true)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Integer id;
+//
+//
+//    @Column(name = "pay_nom")
+//    private String nom ;
+//
+//    @Column(name = "pay_code")
+//    private String code;
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getNom() {
+//        return nom;
+//    }
+//
+//    public void setNom(String nom) {
+//        this.nom = nom;
+//    }
+//
+//    public String getCode() {
+//        return code;
+//    }
+//
+//    public void setCode(String code) {
+//        this.code = code;
+//    }
+//}
