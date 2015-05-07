331df8d7a3bbf4ec907ec31e80cd9792a45e12af
frederic.minatchy <frederik78@yahoo.fr>
2015-05-07 04:42:11 +0200

    Suppression des tests unitaires pour la démo

--- backend/src/main/java/com/techsolcom/carnet/dao/PersonneRepository.java
+++ backend/src/main/java/com/techsolcom/carnet/dao/PersonneRepository.java
@@ -2,8 +2,6 @@ package com.techsolcom.carnet.dao;
 
 import com.techsolcom.dto.carnet.Personne;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 /**
  * Created by Frederic on 03/05/15.
@@ -11,6 +9,4 @@ import org.springframework.data.repository.query.Param;
 
 public interface PersonneRepository extends JpaRepository<Personne, Integer>{
 
-    @Query("Select personne from Personne personne join fetch personne.adressesContact adresse join fetch adresse.typeContact  where personne.id = :id")
-    Personne findPersonAndContactAndProvider(@Param(value = "id") Integer id);
 }
--- backend/src/test/java/com/techsolcom/carnet/dao/TestPersonnesDao.java
+++ backend/src/test/java/com/techsolcom/carnet/dao/TestPersonnesDao.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.MalformedURLException;
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.fest.assertions.api.Assertions.extractProperty;
@@ -33,11 +34,11 @@ public class TestPersonnesDao extends DBTest {
 //        assertThat(extractProperty("pays.nom").from(personnes)).contains("COLOMBIA", "CONGO");
     }
 
-	@Test
-	public void find_contact_and_fetch_addresses_and_provider() {
-        final Personne personne = personneRepository.findPersonAndContactAndProvider(10);
-        assertThat(personne.getPrenom()).isEqualTo("Arnaud");
-		assertThat(personne.getAdressesContact()).hasSize(3);
-	}
+//	@Test
+//	public void find_contact_and_fetch_addresses_and_provider() {
+//        final Personne personne = personneRepository.findPersonAndContactAndProvider(10);
+//        assertThat(personne.getPrenom()).isEqualTo("Arnaud");
+//		assertThat(personne.getAdressesContact()).hasSize(3);
+//	}
 
 }
